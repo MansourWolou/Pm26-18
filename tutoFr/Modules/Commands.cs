9f2ae91d14679d63b8fb14f76901e29d70f210b5
MansourWolou <Wolou0444@gmail.com>
2020-11-05 20:29:37 +0100

    init commit

---
+++ tutoFr/Modules/Commands.cs
@@ -0,0 +1,65 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+
+namespace tutoFr.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>// héritage de tte les méthodes propres a discord
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            var guess = Context.Message.Content;
+            await ReplyAsync("tu as dis " + guess.ToString() + " alors je te répond Pong!") ;
+        }
+        // 
+        [Command("avatar")]
+        public async Task AvatarAsync(ushort size = 512)
+        {
+            await ReplyAsync(Context.User.Username + " la photo de ton avatar est :");
+            await ReplyAsync(CDN.GetUserAvatarUrl(Context.User.Id, Context.User.AvatarId, size, ImageFormat.Auto));
+        }
+
+        [Command("react")]
+        public async Task ReactAsync(string pMessage , string pEmoji)
+        {
+            var message = await Context.Channel.SendMessageAsync(pMessage);
+            var emoji = new Emoji(pEmoji);
+
+            await message.AddReactionAsync(emoji);
+        }
+        [Command("spam")]
+        public async Task SpamAsync()
+        {
+            while (true) 
+            {
+                await ReplyAsync("on est arrivé ? ");
+            } 
+        }
+
+        [Command("StartTchat")]
+        public async Task StartTchatAsync(string pmessage)
+        {
+
+            var umessage = Context.Message.Content;
+            string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: umessage,
+                from: new Twilio.Types.PhoneNumber("+15017122661"),
+                to: new Twilio.Types.PhoneNumber("+15558675310")
+            );
+
+            Console.WriteLine(message.Sid);
+
+        }
+
+    }
+}
---
+++ tutoFr/Program.cs
@@ -0,0 +1,80 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace tutoFr
+{
+    class Program
+    {
+        private DiscordSocketClient client;
+        private CommandService commands;
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        public async Task RunBotAsync()
+        {
+            client = new DiscordSocketClient( new DiscordSocketConfig 
+            { 
+                LogLevel = LogSeverity.Debug
+            });
+
+            commands = new CommandService();
+
+            client.Log += Log;
+
+            client.Ready += () =>
+            {
+                Console.WriteLine("je suis prêt bro");
+                return Task.CompletedTask;
+            };
+
+            await InstallCommandsAsync();
+
+            //attendre que leclient(bot) se connecte en allant chercher le token dans mes variables d'environnements
+            await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("discordToken", EnvironmentVariableTarget.User));
+
+            //attendre que le client démare
+            await client.StartAsync();
+
+            //fait en sorte que le programme ne s trrmine jamais . vu que on veux que notre bot continu de tourner
+            await Task.Delay(-1);
+        }
+        //delimite les comandes supporter
+        public async Task InstallCommandsAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+            // récupère tte nos commandes en cherchant un fichier gérant tt ça dans le rep
+        }
+
+        private async Task HandleCommandAsync(SocketMessage pMessage)
+        {
+            var message = pMessage as SocketUserMessage;//conversion de type pour analyser les messages de l'utilisateur
+
+            if (message == null) return;
+
+            int argPos = 0;//indexation du  premier caractère de la chaine de caractère de la commande
+
+            if (!message.HasCharPrefix('!', ref argPos)) return;
+
+            //création d'un contexte
+            var context = new SocketCommandContext(client, message);
+
+            // gère le resulta de la commande
+            var result = await commands.ExecuteAsync(context, argPos, null);
+
+            // si il y a eu erreur pendant l'exécution de la commande , envoyer un message d'erreur
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
